d908490f2eac629038bb322e2aa867bd1dc30fd3
Prinu Dickson <prinu.dickson@pwc.com>
2024-08-15 09:22:42 +0200

    update

---
+++ appkit4-react-demo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+
+import { CheckboxGroup, Checkbox } from '@appkit4/react-components/checkbox';
+import { Button } from '@appkit4/react-components/button';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Button kind='primary'>Label</Button>
+      {/* TEST TEST! */}
+    </div>
+  );
+}
+
+export default App;
